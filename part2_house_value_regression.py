65d50be862bf277e10ee1673f9b08cab8dd585c8
Shaanukstar123 <shaanukag0@gmail.com>
2022-11-17 17:30:07 +0000

    filling empty data points complete

--- part2_house_value_regression.py
+++ part2_house_value_regression.py
@@ -62,21 +62,49 @@ class Regressor():
         #                       ** START OF YOUR CODE **
         #######################################################################
         
-        #averages to fill in blanks
-        longitude_mean,latitude_mean,income_mean  = 0,0,0#x[["longitude"]].mean(), x[["latitude"]].mean(), x[["median_income"]].mean()
-        age_median,rooms_median,beds_median = 0,0,0#x[["housing_median_age"]].median(), x[["total_rooms"]].median(), x[["population"]].median()
-        population_median,households_median,value_median = 0,0,0#x[["population"]].median(), x[["households"]].median(),x[["median_house_value"]].median()
-        proximity_mode = x[["ocean_proximity"]].mode()
-        print("avg: ",proximity_mode)
-        print("done")
+        # longitude_mean,latitude_mean,income_mean  = x[["longitude"]].mean(), x[["latitude"]].mean(), x[["median_income"]].mean()
+        # age_median,rooms_median,beds_median = x[["housing_median_age"]].median(), x[["total_rooms"]].median(), x[["total_bedrooms"]].median()
+        # population_median,households_median = x[["population"]].median(), x[["households"]].median()
+        # proximity_mode = x[["ocean_proximity"]].mode()
+        # print("avg: ",proximity_mode)
+        # print("done")
+        # empty_columns = x.columns[x.isna().any()].tolist()
+        
 
-        x.fillna({"longitude": longitude_mean,"latitude": latitude_mean,"housing_median_age": age_median,
-            "total_rooms": rooms_median, "total_bedrooms": beds_median, "population": population_median,
-            "households": households_median, "median_income": income_mean, "ocean_proximity": proximity_mode,
-            "median_house_value": value_median
-            },inplace = True)
-        #print(x.apply(lambda x: sum(x.isnull()),axis=0))
+        # average_tbl = {"longitude": longitude_mean,"latitude": latitude_mean,"housing_median_age": age_median,
+        #     "total_rooms": rooms_median, "total_bedrooms": beds_median, "population": population_median,
+        #     "households": households_median, "median_income": income_mean, "ocean_proximity": proximity_mode
+        #     }
+        # # for col in empty_columns:
+        # #     mask = x[col].fillna(average_tbl[col])
+        # #     x.loc[mask,col] #= x[[col]].fillna(average_tbl[col])
+        # #     #x[col].isnull().fillna(average_tbl[col])
+        # print(empty_columns)
+        # for col in empty_columns:
+        #     x[[col]] = x[[col]].fillna(average_tbl[col])
+        #     print(average_tbl[col])
+        # print(x)
 
+
+        #Fills empty data points with averages of their column
+        for col in x:
+            if col ==("longitude" or "latitude" or "median_income"):
+                x[col].fillna(x[col].mean(), inplace=True)
+            elif col == "ocean_proximity":
+                x[col].fillna(x[col].mode()[0], inplace=True)
+            else:
+                x[col].fillna(x[col].median(), inplace=True)
+            #x["ocean_proximity"].fillna(x["ocean_proximity"].median(), inplace=True)
+        # x["longitude"].fillna(x["longitude"].mean(), inplace=True)
+        # x["housing_median_age"].fillna(x["housing_median_age"].median(), inplace=True)
+        # x["total_rooms"].fillna(x["total_rooms"].median(), inplace=True)
+        # x["longitude"].fillna(x["longitude"].mean(), inplace=True)
+        # x["longitude"].fillna(x["longitude"].mean(), inplace=True)
+        # x["longitude"].fillna(x["longitude"].mean(), inplace=True)
+        # x["longitude"].fillna(x["longitude"].mean(), inplace=True)
+        # x["longitude"].fillna(x["longitude"].mean(), inplace=True)
+        
+        print(x)
         proximity_column  = pd.DataFrame(self.bin_labels.fit_transform(x["ocean_proximity"]))
         x = x.drop(columns="ocean_proximity",axis = 1)
         x = x.join(proximity_column)
